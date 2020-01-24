3c2f5ff2fce350f8e549477954bc9b9247716fc8
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2020-01-23 20:35:41 -0800

    gh-3 (feat): add eslint

--- imgur.js
+++ imgur.js
@@ -1,28 +1,28 @@
-const fetch = require('node-fetch')
-const FormData = require('form-data')
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 exports.upload = async (img, title) => {
-  const IMGUR_URI = 'https://api.imgur.com/3/upload'
-  const form = new FormData()
-  form.append('image', img)
-  form.append('title', title)
+  const IMGUR_URI = 'https://api.imgur.com/3/upload';
+  const form = new FormData();
+  form.append('image', img);
+  form.append('title', title);
   const options = {
     method: 'POST',
     headers: {
-        // anonymous
-        // 'Authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+      // anonymous
+      // 'Authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
 
-        // auth, refresh this periodically. See README
-        'Authorization': `Bearer ${process.env.IMGUR_ACCESS_TOKEN}`,
+      // auth, refresh this periodically. See README
+      'Authorization': `Bearer ${process.env.IMGUR_ACCESS_TOKEN}`,
     },
     body: form,
-  }
+  };
 
-  const res = await fetch(IMGUR_URI, options)
-  const json = await res.json()
+  const res = await fetch(IMGUR_URI, options);
+  const json = await res.json();
   if (!json || !json.success) {
-    console.log(json)
-    throw Error('Request failed:' + json.data.error)
+    console.log(json);
+    throw Error('Request failed:' + json.data.error);
   }
-  return json
-}
+  return json;
+};
--- index.js
+++ index.js
@@ -1,125 +1,127 @@
-const puppeteer = require('puppeteer')
-const format = require('date-fns/format')
+const puppeteer = require('puppeteer');
+const format = require('date-fns/format');
 
-const {upload} = require('./imgur')
-const {fetchBox, fetchGames} = require('./nba')
-const {allSettled} = require('./utils')
-const reddit = require('./reddit')
+const {upload} = require('./imgur');
+const {fetchBox, fetchGames} = require('./nba');
+const {allSettled} = require('./utils');
+const reddit = require('./reddit');
 
-const VIEWPORT = {width: 1440, height: 1080}
-const URL = `file://${__dirname}/index.html`
+const VIEWPORT = {width: 1440, height: 1080};
+const URL = `file://${__dirname}/index.html`;
 
 const insertEnv = (page, name, value) => {
-    page.evaluateOnNewDocument(`
+  page.evaluateOnNewDocument(`
         Object.defineProperty(window, '${name}', {
             get() {
                 return ${JSON.stringify(value)}
             }
         })
-    `)
-}
+    `);
+};
 
 const extractGames = (games) => {
-    return games.map(game => {
-        return Object.assign({}, game, {
-            homeCity: game.home.city,
-            homeName: game.home.team_code,
-            homeNickname: game.home.nickname,
-            visitorCity: game.visitor.city,
-            visitorName: game.visitor.team_code,
-            visitorNickname: game.visitor.nickname,
-        })
-    })
-}
+  return games.map((game) => {
+    return Object.assign({}, game, {
+      homeCity: game.home.city,
+      homeName: game.home.team_code,
+      homeNickname: game.home.nickname,
+      visitorCity: game.visitor.city,
+      visitorName: game.visitor.team_code,
+      visitorNickname: game.visitor.nickname,
+    });
+  });
+};
 
 (async () => {
-    // Initialization
-    const browser = await puppeteer.launch()
+  // Initialization
+  const browser = await puppeteer.launch();
 
-    // fetch all the games for date
-    const todayStr = format(new Date(), 'yyyyMMdd')
-    console.log('fetching...', todayStr)
-    let games = await fetchGames(todayStr)
-    if (games == null || games.length === 0) {
-        return;
-    }
+  // fetch all the games for date
+  const todayStr = format(new Date(), 'yyyyMMdd');
+  console.log('fetching...', todayStr);
+  let games = await fetchGames(todayStr);
+  if (games == null || games.length === 0) {
+    return;
+  }
 
-    // search sub reddit's recent post-game threads
-    newPosts = await reddit.getNewPGTs()
-    comments = await reddit.getComments()
-    games = extractGames(games)
-    const promises = games.map(async (game, i) => {
-        console.log('trying to find', game.homeName, game.visitorName)
-        const postGameThread = newPosts.find(post => {
-            const title = post.title.toLowerCase()
-            return (title.includes(game.homeName.toLowerCase()) ||
+  // search sub reddit's recent post-game threads
+  newPosts = await reddit.getNewPGTs();
+  comments = await reddit.getComments();
+  games = extractGames(games);
+  const promises = games.map(async (game, i) => {
+    console.log('trying to find', game.homeName, game.visitorName);
+    const postGameThread = newPosts.find((post) => {
+      const title = post.title.toLowerCase();
+      return (title.includes(game.homeName.toLowerCase()) ||
             title.includes(game.homeCity.toLowerCase()) ||
             title.includes(game.homeNickname.toLowerCase())) &&
             (title.includes(game.visitorName.toLowerCase()) ||
             title.includes(game.visitorCity.toLowerCase()) ||
-            title.includes(game.visitorNickname.toLowerCase()))
-        }
-        )
-        if (postGameThread == null) {
-            console.log(`didn't find ${game.homeName}, ${game.visitorName}`)
-            return
-        }
+            title.includes(game.visitorNickname.toLowerCase()));
+    },
+    );
+    if (postGameThread == null) {
+      console.log(`didn't find ${game.homeName}, ${game.visitorName}`);
+      return;
+    }
 
-        console.log('found thread', postGameThread.id)
-        const hasCommented = comments.find(comment => comment.parent_id === `t3_${postGameThread.id}`) != null
+    console.log('found thread', postGameThread.id);
+    const hasCommented = comments.find(
+        (comment) => comment.parent_id === `t3_${postGameThread.id}`,
+    ) != null;
 
-        console.log('hasCommented', hasCommented)
-        if (hasCommented) {
-            return
-        }
-        console.log('fetching game box', game.id, 'thread', postGameThread.id)
+    console.log('hasCommented', hasCommented);
+    if (hasCommented) {
+      return;
+    }
+    console.log('fetching game box', game.id, 'thread', postGameThread.id);
 
-        const data = await fetchBox(game.id)
-        if (data == null) {
-            return
-        }
+    const data = await fetchBox(game.id);
+    if (data == null) {
+      return;
+    }
 
-        // screenshot
-        console.log('Taking a screenshot...', game.id)
-        const page = await browser.newPage()
-        await page.setViewport(VIEWPORT)
-        insertEnv(page, 'box', data)
-        await page.goto(URL)
-        let element = await page.$('html')
-        const light = await element.screenshot({
-            // encoding: 'base64',
-            path: __dirname + `/images/light-${data.vls.tn} vs ${data.hls.tn}.png`
-        })
+    // screenshot
+    console.log('Taking a screenshot...', game.id);
+    const page = await browser.newPage();
+    await page.setViewport(VIEWPORT);
+    insertEnv(page, 'box', data);
+    await page.goto(URL);
+    let element = await page.$('html');
+    const light = await element.screenshot({
+      // encoding: 'base64',
+      path: __dirname + `/images/light-${data.vls.tn} vs ${data.hls.tn}.png`,
+    });
 
-        // changing to dark mode
-        await page.evaluate(() => {
-            let body = document.querySelector('body');
-            body.className += 'dark'
-        });
-        element = await page.$('html')
-        const dark = await element.screenshot({
-            // encoding: 'base64',
-            path: __dirname + `/images/dark-${data.vls.tn} vs ${data.hls.tn}.png`
-        })
+    // changing to dark mode
+    await page.evaluate(() => {
+      const body = document.querySelector('body');
+      body.className += 'dark';
+    });
+    element = await page.$('html');
+    const dark = await element.screenshot({
+      // encoding: 'base64',
+      path: __dirname + `/images/dark-${data.vls.tn} vs ${data.hls.tn}.png`,
+    });
 
-        console.log('uploading to imgur...')
-        let lightImgurLink;
-        let darkImgurLink;
-        const responses = await allSettled([
-            upload(light, `${data.gdtutc} ${data.vls.tn} vs ${data.hls.tn}`),
-            upload(dark, `${data.gdtutc} ${data.vls.tn} vs ${data.hls.tn}`)
-        ])
-        if (responses[0].status === 'fulfilled') {
-            lightImgurLink = responses[0].value.data.link
-            console.log(lightImgurLink)
-        }
-        if (responses[1].status === 'fulfilled') {
-            darkImgurLink = responses[1].value.data.link
-            console.log(darkImgurLink)
-        }
-        console.log('posting to reddit...')
-        await reddit.postComment(postGameThread, lightImgurLink, darkImgurLink)
-        console.log('finished posting to reddit...')
-    })
-    await allSettled(promises).finally(async () => await browser.close())
-})()
+    console.log('uploading to imgur...');
+    let lightImgurLink;
+    let darkImgurLink;
+    const responses = await allSettled([
+      upload(light, `${data.gdtutc} ${data.vls.tn} vs ${data.hls.tn}`),
+      upload(dark, `${data.gdtutc} ${data.vls.tn} vs ${data.hls.tn}`),
+    ]);
+    if (responses[0].status === 'fulfilled') {
+      lightImgurLink = responses[0].value.data.link;
+      console.log(lightImgurLink);
+    }
+    if (responses[1].status === 'fulfilled') {
+      darkImgurLink = responses[1].value.data.link;
+      console.log(darkImgurLink);
+    }
+    console.log('posting to reddit...');
+    await reddit.postComment(postGameThread, lightImgurLink, darkImgurLink);
+    console.log('finished posting to reddit...');
+  });
+  await allSettled(promises).finally(async () => await browser.close());
+})();
