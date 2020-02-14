e1a5eabb79cafdfbac387c90d77f166dba85768d
Andy Liu <mingchia.andy.liu@gmail.com>
2020-02-14 01:00:41 -0800

    [#15] (fix): build script (#16)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+set -e
+
 rm -rf node_modules
 rm -rf app.zip
 
-zip -r app.zip . -x "*.git*" -x "*node_modules*" -x "*.DS_Store*"
+zip -r app.zip . \
+  -x "*.git*" \
+  -x "*node_modules*" \
+  -x "*.DS_Store*" \
+  -x "*.eslint*" \
+  -x ".env*" \
+  -x "*scripts*"
